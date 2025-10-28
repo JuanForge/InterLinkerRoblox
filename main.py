e55a1975f6d482f12e1c89039a13a6542ae7e9a7
JuanForge <contact.juan.coding@gmail.com>
2025-10-28 22:39:42 +0100

    update

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-import requests
 import queue
+import requests
+
+from concurrent.futures import ThreadPoolExecutor
+
 
 
 def main():
     uername = input("Username>> ")
-    Queue = queue.Queue(maxsize=200)
+    uernameFind = input("Username Find>> ")
+    rq = requests.Session()
+    #Queue = queue.Queue(maxsize=200)
+    data = rq.post("https://users.roblox.com/v1/usernames/users", json={"usernames": [uername], "excludeBannedUsers": False})
+    IDuser = data.json()["data"][0]["id"]
+    print(f"User ID: {IDuser}")
+
+if __name__ == "__main__":
+    main()
