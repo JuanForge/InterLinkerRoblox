6f1761e87ab69bd258167195c6d344cdfa12761d
JuanForge <contact.juan.coding@gmail.com>
2025-10-29 14:46:13 +0100

    update

--- main.py
+++ main.py
@@ -1,53 +1,116 @@
 import sys
 import time
+import json
 import queue
+import yaspin
 import random
 import requests
 import threading
 
+import itertools
+
+from yaspin import spinners
 from concurrent.futures import ThreadPoolExecutor
 
-def unit(rq: requests.Session, uernameFind: str, IDuser: str, Queue: queue.Queue, Set: set) -> dict:
-    if not IDuser in Set:
-        Set.add(IDuser)
+def worker(IDuserFInd: str, Queue: queue.Queue,
+            Set: set, lock: threading.Lock, found_event: threading.Event, result: dict, proxy, nombre: int) -> dict:
+    rq = requests.Session()
+    rq.proxies.update({
+        "http": proxy,
+        "https": proxy
+    })
+    print(rq.proxies)
+    while not found_event.is_set():
         try:
-          data = rq.get(f"https://friends.roblox.com/v1/users/{IDuser}/friends")
-          time.sleep(random.uniform(0.5, 1.5))
+            userQueue = Queue.get(timeout=1)
+        except Exception:
+            continue
+
+        with lock:
+            if userQueue["id"] in Set:
+                Queue.task_done()
+                continue
+            Set.add(userQueue["id"])
+        try:
+            print(f"https://friends.roblox.com/v1/users/{userQueue['id']}/friends")
+            data = rq.get(f"https://friends.roblox.com/v1/users/{userQueue['id']}/friends")
+            #print(data.json())
+            time.sleep(random.uniform(2, 4))
     
-          for user in data.json()["data"]:
-              sys.stdout.write(user["name"].lower() + "\n")
+            for user in data.json()["data"]:
+                with lock:
+                    nombre[0] += 1
+                user["id"] = str(user["id"])
+                sys.stdout.write(user["id"] + "\n")
         
-              if user["name"].lower() == uernameFind.lower():
-                  return {"status": True, "username": user["name"], "id": user["id"], "intermediate": [user["name"]]}
-              else:
-                  iun = unit(rq, uernameFind, user["id"], Queue=Queue, Set=Set)
-                  if iun["status"]:
-                      iun["intermediate"].append(user["name"])
-                      return iun
+                if user["id"] == IDuserFInd:
+                    found_event.set()
+                    with lock:
+                        result.update({
+                            "status": True,
+                            "username": user["name"],
+                            "id": user["id"],
+                            "intermediate": userQueue["intermediate"] + [user["name"]]
+                        })
+                    return True
+                  #return {"status": True, "username": user["name"], "id": user["id"], "intermediate": [user["name"]]}
+                else:
+                  Queue.put({"IDuserFInd": IDuserFInd, "id": user["id"], "intermediate": userQueue["intermediate"] + [user["name"]]})
+                  #iun = unit(rq, uernameFind, user["id"], Queue=Queue, Set=Set)
+                  #if iun["status"]:
+                  #    iun["intermediate"].append(user["name"])
+                  #    return iun
                       #return {"status": True, "username": user["username"], "id": user["id"]}
         except Exception as e:
           print(f"Error: {e}")
-    return {"status": False}
+    return True
+    #return {"status": False}
 
 
 
 def main():
+    thread = 8
+
     uername = input("Username>> ")
     uernameFind = input("Username Find>> ")
-    ThreadPool = ThreadPoolExecutor(max_workers=2)
     lock = threading.Lock()
     rq = requests.Session()
-    Queue = queue.Queue(maxsize=200)
+    Queue = queue.Queue(maxsize=2_000_000)
+    found_event = threading.Event()
+    result = {"status": False}
     Set = set()
+    nombre = [0]
+
     data = rq.post("https://users.roblox.com/v1/usernames/users", json={"usernames": [uername], "excludeBannedUsers": False})
     IDuser = data.json()["data"][0]["id"]
-    print(f"User ID: {IDuser}")
+    print(f"IDuser ID: {IDuser}" + "\n")
 
-    data = unit(rq, uernameFind, IDuser, Queue=Queue, Set=Set, lock=lock)
+    data = rq.post("https://users.roblox.com/v1/usernames/users", json={"usernames": [uernameFind], "excludeBannedUsers": False})
+    IDuserFInd = data.json()["data"][0]["id"]
+    print(f"IDuserFInd ID: {IDuserFInd}" + "\n")
 
-    if data['status']:
-        print("->".join(data["intermediate"][::-1]))
-        #print(f"Found: {user['username']} | ID: {user['id']}")
+    ThreadPool = ThreadPoolExecutor(max_workers=None)
+    Queue.put({"IDuserFInd": IDuserFInd, "id": IDuser, "intermediate": [uername]})
+    
+    proxy = itertools.cycle(json.loads(open("config.json").read())["proxy"])
+    for _ in range(thread):
+        ThreadPool.submit(worker, IDuserFInd, Queue, Set, lock, found_event, result, next(proxy), nombre)
+    print("Searching...")
+    
+    with yaspin.yaspin(spinner=spinners.Spinners.material, text="Process en cours...", color="red", timer=True) as e:
+        while not found_event.is_set():
+            e.text = f"Users scanned: {len(Set)} | Queue size: {Queue.qsize()}, Threads: {threading.active_count()}, nombre: {nombre}"
+            time.sleep(1)
+    
+    print("Found!")
+    print("Found!")
+    ThreadPool.shutdown(wait=True)
+    print(result)
+    #data = worker(rq, uernameFind, IDuser, Queue=Queue, Set=Set, lock=lock, found_event=found_event, result=result)
+
+    #if data['status']:
+    #    print("->".join(data["intermediate"][::-1]))
+    #    #print(f"Found: {user['username']} | ID: {user['id']}")
 
 if __name__ == "__main__":
     main()
