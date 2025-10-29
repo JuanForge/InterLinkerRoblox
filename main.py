288a59da0c09e6287eb479dbecce87017271959f
JuanForge <contact.juan.coding@gmail.com>
2025-10-29 13:03:08 +0100

    update

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import time
 import queue
 import random
 import requests
+import threading
 
 from concurrent.futures import ThreadPoolExecutor
 
-def unit(rq: requests.Session, uernameFind: str, IDuser: str, Cache: list) -> dict:
-    if not IDuser in Cache:
-        Cache.append(IDuser)
+def unit(rq: requests.Session, uernameFind: str, IDuser: str, Queue: queue.Queue, Set: set) -> dict:
+    if not IDuser in Set:
+        Set.add(IDuser)
         try:
           data = rq.get(f"https://friends.roblox.com/v1/users/{IDuser}/friends")
           time.sleep(random.uniform(0.5, 1.5))
@@ -19,7 +20,7 @@ def unit(rq: requests.Session, uernameFind: str, IDuser: str, Cache: list) -> di
               if user["name"].lower() == uernameFind.lower():
                   return {"status": True, "username": user["name"], "id": user["id"], "intermediate": [user["name"]]}
               else:
-                  iun = unit(rq, uernameFind, user["id"], Cache=Cache)
+                  iun = unit(rq, uernameFind, user["id"], Queue=Queue, Set=Set)
                   if iun["status"]:
                       iun["intermediate"].append(user["name"])
                       return iun
@@ -33,14 +34,16 @@ def unit(rq: requests.Session, uernameFind: str, IDuser: str, Cache: list) -> di
 def main():
     uername = input("Username>> ")
     uernameFind = input("Username Find>> ")
-    ThreadPool = ThreadPoolExecutor(max_workers=10)
+    ThreadPool = ThreadPoolExecutor(max_workers=2)
+    lock = threading.Lock()
     rq = requests.Session()
-    #Queue = queue.Queue(maxsize=200)
+    Queue = queue.Queue(maxsize=200)
+    Set = set()
     data = rq.post("https://users.roblox.com/v1/usernames/users", json={"usernames": [uername], "excludeBannedUsers": False})
     IDuser = data.json()["data"][0]["id"]
     print(f"User ID: {IDuser}")
 
-    data = unit(rq, uernameFind, IDuser, Cache=[])
+    data = unit(rq, uernameFind, IDuser, Queue=Queue, Set=Set, lock=lock)
 
     if data['status']:
         print("->".join(data["intermediate"][::-1]))
